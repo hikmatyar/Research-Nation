049bc43023797e50cd440080aff99a9340f7f9d1
Usman Ali <usman.ali@d3velopers.com>
2011-03-21 20:57:14 +0500

    Validations added to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,15 @@ class User < ActiveRecord::Base
   has_many :requests
   has_many :researches
 
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :email
+  validates_presence_of :password
+
+  validates_uniqueness_of :email
+
+  validates_format_of :email, :with => /^([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})$/i, :message => "format is not valid."
+
   def password=(value)
     write_attribute("password", Digest::SHA1.hexdigest(value))
   end
