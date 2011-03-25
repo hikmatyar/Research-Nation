c98e6d5a9c5cc03bbf1a52ea2f495fbc87dd7819
Laiq <usman.ali@d3velopers.com>
2011-03-25 18:21:57 +0500

    Added facebook keys for production and added mail delivery method

--- config/environment.rb
+++ config/environment.rb
@@ -18,7 +18,7 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
-  # config.gem "aws-s3", :lib => "aws/s3"
+  # config.gem "aws-s3", :lib => "aws/s3"'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
@@ -39,3 +39,6 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 end
+
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.delivery_method = :sendmail
