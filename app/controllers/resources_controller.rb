4710c764577301b7db16fa771f27f507392f8b46
Laiq <usman.ali@d3velopers.com>
2011-03-29 20:49:00 +0500

    Resources being saved through model rather than controller

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -11,66 +11,9 @@ class ResourcesController < ApplicationController
     end
   end
 
-  def create
-    @user = User.find( session[:user] )
-    resource = Resource.new params[:post]
-    resource.user_id = @user.id
-    if resource.save
-      documents = params[:document]
-      unless documents.blank?
-        documents.each do |key, doc|
-          if upload_to_s3(doc)
-            attachment = Attachment.new
-            attachment.file_name = File.basename(doc.original_filename) + "_#{@user.id.to_s}"
-            attachment.resource_id = resource.id
-            attachment.save
-          else
-            resource.destroy
-            flash[:error] = "Error While Uploading Files."
-            return redirect_to :action => 'post'
-          end
-        end
-      flash[:error] = ""
-      flash[:success] = "Your Research Has been Uploaded Successfully"
-      return redirect_to :controller => 'main', :action => 'index'
-      else
-        resource.destroy
-        flash[:success] = ""
-        flash[:error]= "Please attach one or more files"
-        return redirect_to :action => 'post'
-      end
-    else
-      flash[:success] = ""
-      flash[:error] = "Error while saving your Research. Please review your form."
-      return redirect_to :action => 'post'
-    end
-  end
-
   def what_others_are_selling
     @user = User.find(session[:user]) if logged_in?
     @resource = Resource.find(params[:id])
   end
 
-  private
-
-  def upload_to_s3(doc)
-    s3_settings = YAML::load(File.open("#{RAILS_ROOT}/config/s3.yml"))
-    bucket = s3_settings[RAILS_ENV]['bucket']
-    mime_type = doc.content_type || "application/octet-stream"
-
-    AWS::S3::Base.establish_connection!(
-      :access_key_id     => s3_settings[RAILS_ENV]['access_key'],
-      :secret_access_key => s3_settings[RAILS_ENV]['secret_key']
-    )
-
-    base_name =  "_#{@user.id.to_s}" + File.basename(doc.original_filename)
-
-    obj = AWS::S3::S3Object.store(
-      base_name,
-      doc,
-      bucket,
-      :content_type => mime_type
-    )
-    return true if obj
-  end
 end
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -5,4 +5,9 @@ class Resource < ActiveRecord::Base
   validates_presence_of :selling_price
   validates_presence_of :title
   validates_numericality_of :selling_price
+
+  def self.create_resource resource_data
+    resource = resource_data
+    resource.save
+  end
 end
