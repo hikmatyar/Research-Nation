b1ba67a87b70725c7603014d57853b7f06e52bf6
Laiq <usman.ali@d3velopers.com>
2011-03-29 20:46:37 +0500

    Requests now being created in model. Title also added to requests

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -2,22 +2,6 @@ class RequestsController < ApplicationController
 
   layout 'main'
 
-  def create
-    if logged_in?
-      request = Request.new(params[:request])
-      request.user_id = session[:user]
-      if request.save
-        flash[:success] = "Your Request has been submitted"
-        return redirect_to :controller => 'main' , :action => 'index'
-      else
-        flash[:error] = "Request Could not be processed. Please try again."
-        return redirect_to :controller => 'main', :action => 'index'
-      end
-    else
-      return redirect_to :controller => 'users', :action => 'register'
-    end
-  end
-
   def what_others_are_requesting
     @request = Request.find(params[:id])
     @user = User.find(session[:user]) if logged_in?
--- app/models/request.rb
+++ app/models/request.rb
@@ -4,4 +4,9 @@ class Request < ActiveRecord::Base
 
   validates_presence_of :description
 
+  def self.create_request request_data
+    request = request_data
+    request.save
+  end
+
 end
