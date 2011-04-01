48e8f6e34560d2e63821b59d39922154839cc90b
Laiq <usman.ali@d3velopers.com>
2011-04-01 19:20:43 +0500

    Download sample implementation

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -21,7 +21,23 @@ class ResourcesController < ApplicationController
   def delete
     resource = Resource.find(params[:id])
     resource.destroy
-    return render :partial => '/layouts/selling_list'
+    @resources = Resource.paginate :page => params[:page], :order => 'created_at DESC'
+    render :layout => false
   end
 
+  def download
+    resource_id = params[:id]
+    resource = Resource.find resource_id
+    sample = resource.attachments.find_by_attachment_type("sample")
+    s3_settings = YAML::load(File.open("#{RAILS_ROOT}/config/s3.yml"))
+    bucket = s3_settings[RAILS_ENV]['bucket']
+    AWS::S3::Base.establish_connection!(
+      :access_key_id     => s3_settings[RAILS_ENV]['access_key'],
+      :secret_access_key => s3_settings[RAILS_ENV]['secret_key']
+    )
+
+    obj = AWS::S3::S3Object.find(sample.file_name, bucket)
+    return redirect_to obj.url
+    return redirect_to :controller => 'main', :action => 'index'
+  end
 end
