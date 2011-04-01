a4fa8242ddc5752dd4db5d23daaada8a92eb4859
Laiq <usman.ali@d3velopers.com>
2011-04-01 10:47:41 +0500

    Showing errors in post form

--- public/javascripts/researchnation.js
+++ public/javascripts/researchnation.js
@@ -7,39 +7,42 @@ function validate(){
   var details = jQuery("#details").val();
   var price = jQuery("#price").val();
 
-  if (jQuery("#option").val()=="Select One")
+  /*if (jQuery("#option").val()=="Select One")
   {
     jQuery("#option").css('border','5px solid #7F7F7F');
     status = false;
-  }
+  }*/
   if (user_name=="")
   {
-
     jQuery("#name").css('border','3px solid #7F7F7F');
+    jQuery("#name").after("<i style='color:red'>Please enter your name</i>");
     status = false;
   }
 
   if (email =="" || !email.match(/^([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})$/i))
   {
-
+    jQuery("#email").after("<i style='color:red'>Email should not be blank or invalid");
     jQuery("#email").css('border','3px solid #7F7F7F');
     status = false;
   }
 
   if (title=="")
   {
+    jQuery("#title").after("<i style='color:red'>Title should not be left blank");
     jQuery("#title").css('border','3px solid #7F7F7F');
     status = false;
   }
 
-  if (details=="")
+  if (details.length > 700)
   {
+    jQuery("#details").after("<i style='color:red'>Details should not be more than 700 characters");
     jQuery("#details").css('border','3px solid #7F7F7F');
     status = false;
   }
 
   if (price == "" && !jQuery("#sell_fields").attr('style')=="display: none;" || Number(price) > 1000 ||  !Number(price))
   {
+    jQuery("#price").after("<i style='color:red'>Price should not be blank or greater than 1000");
     jQuery("#price").css('border','3px solid #7F7F7F');
     status = false;
   }
@@ -48,10 +51,10 @@ function validate(){
 
 jQuery(document).ready(function(){
 
-  list = location.href.split("=")[1]
-  if (!list){
+  list = "selling_list" //location.href.split("=")[1]
+  /*if (!list){
     list = "buying_list"
-  }
+  }*/
   jQuery('#option').change(function(){
     if(jQuery(this).val()=="Buy"){
       jQuery('#sell_fields').hide();
