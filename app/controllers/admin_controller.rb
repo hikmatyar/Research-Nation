a6cf0bf0be920c7554a0476013fee4477d632bc8
Ali Ibrahim <aliibrahim@gmail.com>
2011-04-05 13:07:07 +0500

    added user management + make users admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -12,4 +12,13 @@ class AdminController < ApplicationController
     @users =  User.find :all, :conditions => ['facebook_uid > ?', 0 ]
   end
 
+  def manage_users
+    @users = User.paginate :page => params[:page]
+  end
+
+  def change_admin_status
+    user = User.find(:first, :conditions => ["id = ?", params[:user_id]])
+    return render :partial => "is_admin_field", :locals => {:user => user}
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,9 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :email, :message =>"has already been registered"
   validates_format_of :email, :with => /^([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})$/i, :message => "format is not valid"
 
+  cattr_reader :per_page
+  @@per_page = 10
+
   def password=(value ="")
     if value.length >=6 && value.length <= 20
       write_attribute("password", Digest::SHA1.hexdigest(value))
@@ -18,7 +21,11 @@ class User < ActiveRecord::Base
       self.errors.add("Password should be between 6 to 20 characters")
     end
   end
-  
+
+  def change_admin_status
+    self.update_attributes :is_admin => !self.is_admin
+  end
+
   def self.create_facebook_user(first_name, last_name, email, id, password, gender, birthday,location )
     user = User.new
     user.first_name = first_name
