44101c8ca0b990d8bd16f4ebda4b41ddc2253084
Laiq <usman.ali@d3velopers.com>
2011-04-09 19:39:17 +0500

    added password reset mail

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -10,4 +10,15 @@ class UserMailer < ActionMailer::Base
                    :email => email
     sent_on Time.now
   end
+  
+  def password_reset_email(user, token)
+
+      recipients   user.email
+      subject      "ResearchNation Password Reset"
+      from         "no-reply@researchnation.net"
+      content_type "text/html"
+      body         :email => user.email,
+                   :url => "http://amazing.pk/users/new_password/#{user.id}?email=#{user.email}&token=#{token}"
+    sent_on Time.now
+  end
 end
