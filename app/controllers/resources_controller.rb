5d8ba2b17f43cd06fd2709835229d3cdc1e2f7c5
Laiq <usman.ali@d3velopers.com>
2011-04-09 19:45:14 +0500

    added layout for resource controller and modified implementation according to new design

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,21 +1,24 @@
 class ResourcesController < ApplicationController
 
-  layout 'main'
   require 'aws/s3'
+  require 'prawn'
+  #before_filter :redirect_to_login, :only => [:delete, edit]
 
-  before_filter :redirect_to_login
-
-  def post
-    if logged_in?
-      @user = User.find(session[:user])
-    else
-      redirect_to :controller => 'main', :action => 'index'
-    end
+  def new
+    @user = User.find(session[:user]) if logged_in?
   end
 
-  def what_others_are_selling
+  def post
+      user = logged_in? ? (User.find(session[:user])) : (User.new(params[:user]))
+      user.update_attributes(params[:user]) if logged_in?
+      puts user.inspect
+      return redirect_to :action => 'upload_docs', :user => ( logged_in? ? user : params[:user] )
+  end
+
+  def seller_page
     @user = User.find(session[:user]) if logged_in?
     @resource = Resource.find(params[:id])
+    @sample = @resource.attachments.sample.first
   end
 
   def delete
@@ -29,20 +32,21 @@ class ResourcesController < ApplicationController
     @resource = Resource.find(params[:id])
   end
 
-  def download
-    resource_id = params[:id]
-    resource = Resource.find resource_id
-    sample = resource.attachments.find_by_attachment_type("sample")
-    s3_settings = YAML::load(File.open("#{RAILS_ROOT}/config/s3.yml"))
-    bucket = s3_settings[RAILS_ENV]['bucket']
-    AWS::S3::Base.establish_connection!(
-      :access_key_id     => s3_settings[RAILS_ENV]['access_key'],
-      :secret_access_key => s3_settings[RAILS_ENV]['secret_key']
-    )
+  def view_posts
+    @user = User.find(session[:user]) if logged_in?
+    @resources = Resource.paginate :page => params[:page], :order => 'created_at DESC'
+  end
 
-    obj = AWS::S3::S3Object.find(sample.file_name, bucket)
-    return redirect_to obj.url
-    return redirect_to :controller => 'main', :action => 'index'
+  def create_post
+  	if logged_in?
+    	resource = Resource.new(params[:resource])
+    	resource.user_id = session[:user]
+    	if resource.save
+    	  Attachment.add_file( params[:sample], resource.id, "sample" )
+    	  Attachment.add_file( params[:original], resource.id, "original" )
+	    return redirect_to :action => 'seller_page', :id => resource.id
+	    end
+    end
   end
 
   def update
@@ -55,4 +59,5 @@ class ResourcesController < ApplicationController
     return redirect_to :controller => 'main', :action => 'index'
 
   end
+
 end
