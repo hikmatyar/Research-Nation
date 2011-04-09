0c3a1855abf8de52aab606c20c5304de65f11589
Laiq <usman.ali@d3velopers.com>
2011-04-09 19:41:29 +0500

    show tab being manipulated throug jquery

--- public/javascripts/researchnation.js
+++ public/javascripts/researchnation.js
@@ -1,84 +1,30 @@
-jQuery(document).ready(function(){
+function showTab(value) 
+	{
+		if(value == "signup")
+		{
+			document.getElementById('tabOne').style.display = "block"
+			document.getElementById('tabTwo').style.display = "none"
 
-  new Ajax.Updater('results', '/main/selling_list' , {asynchronous:true, evalScripts:true});
-  jQuery("form").submit(function(){
- 
-  jQuery(".error").each(function(){
-    jQuery(this).text("");
-  })
+			document.getElementById('tab1').style.background = "#FFFFFF"
+			document.getElementById('tab1').style.color = "#245a83"
 
-  var user_name = jQuery("#name").val();
-  var email = jQuery("#email").val();
-  var title = jQuery("#title").val();
-  var details = jQuery("#details").val();
-  var price = jQuery("#price").val();
- 
-  jQuery("#name").css('border','1px solid #7F7F7F');
-  jQuery("#email").css('border','1px solid #7F7F7F');
-  jQuery("#title").css('border','1px solid #7F7F7F');
-  jQuery("#details").css('border','1px solid #7F7F7F');
-  jQuery("#price").css('border','1px solid #7F7F7F');
+			document.getElementById('tab2').style.background = "#bfbfb5"
+			document.getElementById('tab2').style.color = "#FFFFFF"
 
-  var status = true; 
-  if (user_name=="" || Number(user_name))
-  {
-    jQuery("#name").css('border','3px solid red');
-    jQuery("#name").after("<i class='error'>Name Should not be blank or a number</i>");
-    status = false;
-  }
+			return true;
+		}
+		if(value == "login")
+		{
+			document.getElementById('tabOne').style.display = "none"
+			document.getElementById('tabTwo').style.display = "block"
 
-  if (email =="" || !email.match(/^([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})$/i))
-  {
-    jQuery("#email").after("<i class='error'>Email should not be blank or invalid");
-    jQuery("#email").css('border','3px solid red');
-    status = false;
-  }
+			document.getElementById('tab2').style.background = "#FFFFFF"
+			document.getElementById('tab2').style.color = "#245a83"
 
-  if (title=="" || title.length > 140)
-  {
-    jQuery("#title").after("<i class='error'>Title should not be left blank or greater than 140 characters");
-    jQuery("#title").css('border','3px solid red');
-    status = false;
-  }
+			document.getElementById('tab1').style.background = "#bfbfb5"
+			document.getElementById('tab1').style.color = "#FFFFFF"
 
-  if (details.length > 700)
-  {
-    jQuery("#details").after("<i class='error'>Details should not be more than 700 characters");
-    jQuery("#details").css('border','3px solid red');
-    status = false;
-  }
-
-  if ((price == "" && !jQuery("#sell_fields").attr('style')=="display: none;") || Number(price) > 1000 ||  !Number(price))
-  {
-    jQuery("#price").after("<i class='error'>Price should not be blank or greater than 1000");
-    jQuery("#price").css('border','3px solid red');
-    status = false;
-  }
-
-  if(status==true){
-    return true;
-  }
-  return false;
-  });
-  list = "selling_list"
-
-  jQuery('#option').change(function(){
-    if(jQuery(this).val()=="Buy"){
-      jQuery('#sell_fields').hide();
-    }
-    else
-    {
-      jQuery('#sell_fields').show();
-    }
-  });
-  jQuery("#buying").click(function(){
-      jQuery('#sell_fields').hide();
-      new Ajax.Updater('results', '/main/selling_list', {asynchronous:true, evalScripts:true});
-    });
-  jQuery("#selling").click(function(){
-    jQuery('#sell_fields').show();
-    new Ajax.Updater('results', '/main/selling_list', {asynchronous:true, evalScripts:true});
-  });
-
-});
+			return true;
+		}
+	}
 
