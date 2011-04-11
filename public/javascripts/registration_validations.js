3f12606e2e71e23c664560482e7f72fa217d482e
Laiq <usman.ali@d3velopers.com>
2011-04-11 19:12:30 +0500

    validations being done without using any plugin

--- public/javascripts/registration_validations.js
+++ public/javascripts/registration_validations.js
@@ -1,22 +1,29 @@
 $(document).ready(function(){
+
 	showTab(location.href.split("=")[1]);
 
-	var first_name_validation = new LiveValidation('first_name');
-	first_name_validation.add( Validate.Presence );
-	first_name_validation.add( Validate.Length, { minimum : 2 } );
-
-	var last_name_validation = new LiveValidation('last_name');
-	last_name_validation.add( Validate.Presence );
-	last_name_validation.add( Validate.Length, { minimum : 2 } );
-
-	var email_validation = new LiveValidation('email');
-	email_validation.add( Validate.Presence );
-	email_validation.add( Validate.Email );
-
-	var password_validation = new LiveValidation('password');
-	password_validation.add( Validate.Presence );
-	password_validation.add( Validate.Length,{ minimum : 6 } );
-
+	$("#new_user").submit(function(){
+			$('.error').text("");
+			valid = true;
+			if($("#first_name").val()==""){
+				$("#first_name").after("<p class='error'>First Name cannot be blank</p>");
+			}
+			if($("#last_name").val()==""){
+				$("#last_name").after("<p class='error'>Last Name cannot be blank</p>");
+			}
+			if($("#email").val()==""){
+				$("#email").after("<p class='error'>Email cannot be blank</p>");
+			}
+			if(!($("#email").val()==$("#email").val().match(/^([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})/)[0])){
+				$("#email").after("<p class='error'>Email format is invalid</p>");
+			}
+			if($("#password").val().length < 6 || $("#password").val() == ""){
+				$("#password").after("<p class='error'>Password must be at least 6 characters long</p>");
+			}
+			if($('.error').length>0){
+				return false;
+			}
+	});
 
 	$(".check_box input:checkbox").change(function(){
 		if (!($(this).is(":checked"))){
