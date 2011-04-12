903a8ab1946b65c45e7697c800b7eea549b22910
Laiq <usman.ali@d3velopers.com>
2011-04-12 18:59:29 +0500

    added custom validations for forms

---
+++ public/javascripts/custom_validations.js
@@ -0,0 +1,54 @@
+function validate_form(){
+	$('.error').remove();
+	$('.error_field').removeClass("error_field");
+
+	$(".required").each(function(){
+		if($(this).val()=="")
+		{
+			$(this).after("<span class='error'>Field Cannot be blank</span>");
+			$(this).addClass("error_field");
+		}
+	});
+
+	$(".email").each(function(){
+		if ($(this).val() != $(this).val().match(/^([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})/)[0]){
+			$(this).after("<span class='error'>Please enter a valid email address</span>");
+			$(this).addClass("error_field");
+		}
+	});
+
+	$(".max_len").each(function(){
+		len = Number($(this).attr("max_length"));
+		if ($(this).val().length > len){
+			$(this).after("<span class='error'>Length exceeded</span>");
+			$(this).addClass("error_field");
+		}
+	});
+
+$(".file").each(function(){
+		
+		file_name = $(this).val();
+		if (!file_name.match(/^.*\.(doc|DOC|ppt|PPT|xls|xls|pdf|PDF|docx|DOCX|pptx|PPTX|xlsx|XLSX)/))
+		{
+			$(this).after("<span class='error'>Please select a valid File format</span>");
+			$(this).addClass("error_field");
+		}
+	});
+
+	$(".min_len").each(function(){
+		len = Number($(this).attr("min_length"));
+		if ($(this).val().length < len){
+			$(this).after("<span class='error'>Length should not be less than "+ len +" characters</span>");
+			$(this).addClass("error_field");
+		}
+	});
+
+	if($(".error").length > 0)
+	{
+		return false;
+	}
+	else{
+		image = '<img src="/images/loading_bar.gif" />'
+		$('.register_form').append(image)
+	}
+}
