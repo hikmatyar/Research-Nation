638278e048c34049358253b2b0fa379e68c589fe
Laiq <usman.ali@d3velopers.com>
2011-04-13 14:41:20 +0500

    fixed the name issues in seller page tabs

--- public/javascripts/seller.js
+++ public/javascripts/seller.js
@@ -1,29 +1,29 @@
 		jQuery(document).ready(function(){
 
-		jQuery("#tab2").click(function(){
+		jQuery("#tab1").click(function(){
 			jQuery("#tabOne").show();
 			jQuery("#tabTwo").hide();
 			jQuery("#tabThree").hide();
 
-			jQuery('#tab1').css("background" ,"#FFFFFF");
-			jQuery('#tab1').css("color", "#245a83");
+			jQuery('#tab2').css("background" ,"#FFFFFF");
+			jQuery('#tab2').css("color", "#245a83");
 
-			jQuery('#tab2').css("background", "#bfbfb5");
-			jQuery('#tab2').css("color", "#FFFFFF");
+			jQuery('#tab1').css("background", "#bfbfb5");
+			jQuery('#tab1').css("color", "#FFFFFF");
 
 			jQuery('#tab3').css("background", "#FFFFFF");
 			jQuery('#tab3').css("color", "#245a83");
 		});
-		jQuery("#tab1").click(function(){
+		jQuery("#tab2").click(function(){
 			jQuery("#tabOne").hide();
 			jQuery("#tabTwo").show();
 			jQuery("#tabThree").hide();
 			
-			jQuery('#tab1').css("background", "#bfbfb5");
-			jQuery('#tab1').css("color", "#FFFFFF");
+			jQuery('#tab2').css("background", "#bfbfb5");
+			jQuery('#tab2').css("color", "#FFFFFF");
 
-			jQuery('#tab2').css("background" ,"#FFFFFF");
-			jQuery('#tab2').css("color", "#245a83");
+			jQuery('#tab1').css("background" ,"#FFFFFF");
+			jQuery('#tab1').css("color", "#245a83");
 
 			jQuery('#tab3').css("background", "#FFFFFF");
 			jQuery('#tab3').css("color", "#245a83");
@@ -33,11 +33,11 @@
 			jQuery("#tabTwo").hide();
 			jQuery("#tabThree").show();
 			
-			jQuery('#tab1').css("background", "#FFFFFF");
-			jQuery('#tab1').css("color", "#245a83");
-
-			jQuery('#tab2').css("background" ,"#FFFFFF");
+			jQuery('#tab2').css("background", "#FFFFFF");
 			jQuery('#tab2').css("color", "#245a83");
+
+			jQuery('#tab1').css("background" ,"#FFFFFF");
+			jQuery('#tab1').css("color", "#245a83");
 
 			jQuery('#tab3').css("background", "#bfbfb5");
 			jQuery('#tab3').css("color", "#FFFFFF");
