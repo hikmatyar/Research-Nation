8370833be93c4641f8578ddc9839346cd2472ec7
Laiq <usman.ali@d3velopers.com>
2011-04-14 20:12:04 +0500

    changed header and footer

--- public/javascripts/custom_validations.js
+++ public/javascripts/custom_validations.js
@@ -1,65 +1,65 @@
 function validate_form(){
-	$('.error').remove();
-	$('.error_field').removeClass("error_field");
+	jQuery('.error').remove();
+	jQuery('.error_field').removeClass("error_field");
 
-	$(".required").each(function(){
-		if($(this).val()=="")
+	jQuery(".required").each(function(){
+		if(jQuery(this).val()=="")
 		{
-			$(this).after("<span class='error'>Field Cannot be blank</span>");
-			$(this).addClass("error_field");
+			jQuery(this).after("<span class='error'>Field Cannot be blank</span>");
+			jQuery(this).addClass("error_field");
 		}
 	});
 
-	$(".email").each(function(){
-		if (!($(this).hasClass("error_field")) && $(this).val() != $(this).val().match(/^([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})/)[0]){
-		  $(this).after("<span class='error'>Please enter a valid email address</span>");
-		  $(this).addClass("error_field");
+	jQuery(".email").each(function(){
+		if (!(jQuery(this).hasClass("error_field")) && jQuery(this).val() != jQuery(this).val().match(/^([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})/)[0]){
+		  jQuery(this).after("<span class='error'>Please enter a valid email address</span>");
+		  jQuery(this).addClass("error_field");
 	  }
 	});
 
-	$(".max_len").each(function(){
-		len = Number($(this).attr("max_length"));
-		if ($(this).val().length > len){
-			$(this).after("<span class='error'>Length exceeded</span>");
-			$(this).addClass("error_field");
+	jQuery(".max_len").each(function(){
+		len = Number(jQuery(this).attr("max_length"));
+		if (jQuery(this).val().length > len){
+			jQuery(this).after("<span class='error'>Length exceeded</span>");
+			jQuery(this).addClass("error_field");
 		}
 	});
-$(".file").each(function(){
+jQuery(".file").each(function(){
 		
-		file_name = $(this).val();
+		file_name = jQuery(this).val();
 		if (file_name && !file_name.match(/^.*\.(doc|DOC|ppt|PPT|xls|xls|pdf|PDF|docx|DOCX|pptx|PPTX|xlsx|XLSX)/))
 		{
-			$(this).after("<span class='error'>Please select a valid File format</span>");
-			$(this).addClass("error_field");
+			jQuery(this).after("<span class='error'>Please select a valid File format</span>");
+			jQuery(this).addClass("error_field");
 		}
 	});
-	$(".min_len").each(function(){
-		len = Number($(this).attr("min_length"));
-		if ($(this).val().length < len){
-			$(this).after("<span class='error'>Length should not be less than "+ len +" characters</span>");
-			$(this).addClass("error_field");
+	jQuery(".min_len").each(function(){
+		len = Number(jQuery(this).attr("min_length"));
+		if (jQuery(this).val().length < len){
+			jQuery(this).after("<span class='error'>Length should not be less than "+ len +" characters</span>");
+			jQuery(this).addClass("error_field");
 		}
 	});
 
-	$(".valid_price").each(function(){
-		price = Number($(this).val());
+	jQuery(".valid_price").each(function(){
+		price = Number(jQuery(this).val());
 		if(price < 0 || price > 1000){
-			$(this).after("<span class='error'>Price range should be between 0 and 1000</span>");
-			$(this).addClass("error_field");
+			jQuery(this).after("<span class='error'>Price range should be between 0 and 1000</span>");
+			jQuery(this).addClass("error_field");
 		}
 		if (String(price).match(/[.]/))
 		{
-			$(this).after("<span class='error'>Price should be whole number</span>");
-			$(this).addClass("error_field");
+			jQuery(this).after("<span class='error'>Price should be whole number</span>");
+			jQuery(this).addClass("error_field");
 		}
 	});
 
-	if($(".error").length > 0)
+	if(jQuery(".error").length > 0)
 	{
 		return false;
 	}
 	else{
 		image = '<img src="/images/loading_bar.gif" />'
-		$('.register_form').append(image);
+		jQuery('.register_form').append(image);
 	}
 }
