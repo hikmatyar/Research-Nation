0207e70b77ab071fb30e676c1ebb844452bdf302
Laiq <usman.ali@d3velopers.com>
2011-04-14 20:00:24 +0500

    new admin module added

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,10 +2,31 @@ class AdminController < ApplicationController
 
   before_filter :redirect_to_admin_login
 
-  layout 'main'
-
   def dashboard
+
+    @users = User.paginate :page => params[:page]
     @resources = Resource.all
+
+    @resources_count = Resource.count
+    @users_count = User.count
+    @admin_count = (User.find_all_by_is_admin true).count
+
+  end
+
+  def users
+    @users = User.paginate :page => params[:page]
+  end
+
+  def posts
+    @posts = Resource.paginate :page => params[:page]
+  end
+
+  def mail_chimp_list
+    @members = get_mailchimp_list_members
+  end
+
+  def admin_rights
+    @users = User.paginate :page => params[:page]
   end
 
   def see_authentications
