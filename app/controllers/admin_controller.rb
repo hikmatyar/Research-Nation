815615c1d67f83067268fc9170f408d4228cbdce
Laiq <usman.ali@d3velopers.com>
2011-04-15 19:51:32 +0500

    controller changes to admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,24 +1,21 @@
 class AdminController < ApplicationController
 
-  before_filter :redirect_to_admin_login
+  before_filter :redirect_to_admin_login, :get_counts
 
   def dashboard
-
     @users = User.paginate :page => params[:page]
     @resources = Resource.all
-
-    @resources_count = Resource.count
-    @users_count = User.count
-    @admin_count = (User.find_all_by_is_admin true).count
-
   end
 
   def users
-    @users = User.paginate :page => params[:page]
+    @users = User.paginate :page => params[:page], :order => 'created_at DESC'
   end
 
+  def experts
+    @users= User.paginate :page => params[:page], :order => 'created_at DESC', :conditions => ['is_expert = ?', true]
+  end
   def posts
-    @posts = Resource.paginate :page => params[:page]
+    @posts = Resource.paginate :page => params[:page], :order => 'created_at DESC'
   end
 
   def mail_chimp_list
@@ -26,7 +23,7 @@ class AdminController < ApplicationController
   end
 
   def admin_rights
-    @users = User.paginate :page => params[:page]
+    @users = User.paginate :page => params[:page], :order => 'created_at DESC'
   end
 
   def see_authentications
@@ -40,6 +37,15 @@ class AdminController < ApplicationController
   def change_admin_status
     user = User.find(:first, :conditions => ["id = ?", params[:user_id]])
     return render :partial => "is_admin_field", :locals => {:user => user}
+  end
+
+  def get_counts
+    session[:members_count] = get_mailchimp_list_members.count if session[:members_count].blank?
+    @resources_count = Resource.count
+    @users_count = User.count
+    @admin_count = (User.find_all_by_is_admin true).count
+    @memebers_count = session[:members_count]
+    @experts_count = (User.find_all_by_is_expert(true)).count
   end
 
 end
