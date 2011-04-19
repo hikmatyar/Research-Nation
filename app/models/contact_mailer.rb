c2892ab190ffbff9db976fe3a1c508b49c45127c
Usman Ali <usman.ali@d3velopers.com>
2011-04-19 20:20:16 +0500

    designed email format for contact mailers

--- app/models/contact_mailer.rb
+++ app/models/contact_mailer.rb
@@ -2,18 +2,18 @@ class ContactMailer < ActionMailer::Base
 
   def feedback_email message
     recipients   "admin@researchnation.net"
-    subject      "Feedback From User"
+    subject      "Feedback - Sent from About Us Page"
     from         "no-reply@researchnation.net"
     content_type "text/html"
     body         :message => message
   end
 
-  def contact_us_email name, email, subject, message
+  def contact_us_email name, email, mail_subject, message
     recipients   "admin@researchnation.net"
-    subject      "Question From User"
+    subject      "Feedback - Sent from Contact Us Page"
     from         "no-reply@researchnation.net"
     content_type "text/html"
-    body         :name => name, :email => email, :subject => subject, :message => message
+    body         :name => name, :email => email, :mail_subject => mail_subject, :message => message
   end  
 
 end
