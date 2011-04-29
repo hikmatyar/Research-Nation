eb42a54dfb91593b0eb9b90b8771a72a780852d3
Ali Ibrahim <aliibrahim@gmail.com>
2011-04-29 17:04:44 +0500

    image paths updated with paperclip urls

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -12,7 +12,6 @@ class Profile < ActiveRecord::Base
                     :path => "photos/:id/:style/:basename.:extension",
                     :bucket => IMAGE_BUCKET
 
-  validates_attachment_presence :photo
   validates_attachment_size :photo, :less_than => 5.megabytes
   validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png', 'image/gif']
 
