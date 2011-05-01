2682cc31510f1481b0d16a314cc4c18ef9456ee9
Ali Ibrahim <aliibrahim@gmail.com>
2011-05-01 16:17:16 +0500

    unused function in resource model

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -18,20 +18,4 @@ class Resource < ActiveRecord::Base
     resource.save
   end
 
-  def download
-    resource_id = params[:id]
-    resource = Resource.find resource_id
-    sample = resource.attachments.find_by_attachment_type("sample")
-    s3_settings = YAML::load(File.open("#{RAILS_ROOT}/config/s3.yml"))
-    bucket = s3_settings[RAILS_ENV]['bucket']
-    AWS::S3::Base.establish_connection!(
-      :access_key_id     => s3_settings[RAILS_ENV]['access_key'],
-      :secret_access_key => s3_settings[RAILS_ENV]['secret_key']
-    )
-
-    obj = AWS::S3::S3Object.find(sample.file_name, bucket)
-    return redirect_to obj.url
-    return redirect_to :controller => 'main', :action => 'index'
-  end
-
 end
