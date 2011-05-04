a3bbc7bddbe11bcf186ea800cf596a0f84b1da0b
Usman Ali <usman.ali@d3velopers.com>
2011-05-04 17:53:39 +0500

    refactored javascript for seller page

--- public/javascripts/seller.js
+++ public/javascripts/seller.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function(){
 
+ jQuery("#summary_link").addClass("active");
  jQuery("#message_link").fancybox({
     'hideOnOverlayClick' : false
   });
@@ -8,82 +9,97 @@ jQuery(document).ready(function(){
   {
     jQuery("#message_link").trigger("click");
   }
-	jQuery("#tab1").click(function(){
-		jQuery("#tabOne").show();
-		jQuery("#tabTwo").hide();
-		jQuery("#tabThree").hide();
-		jQuery("#tabFour").hide();
+	jQuery("#summary_link").click(function(){
+		jQuery(".seller_blocks").hide();
+		jQuery("#summary_details").show();
+		/*jQuery("#about_seller").hide();
+		jQuery("#view_sample").hide();
+		jQuery("#sources").hide();*/
 
-		jQuery('#tab2').css("background" ,"#FFFFFF");
-		jQuery('#tab2').css("color", "#245a83");
+		/*jQuery('#seller_link').css("background" ,"#FFFFFF");
+		jQuery('#seller_link').css("color", "#245a83");
 
-		jQuery('#tab1').css("background", "#bfbfb5");
-		jQuery('#tab1').css("color", "#FFFFFF");
-		jQuery('#tab1').addClass("active");
+		jQuery('#summary_link').css("background", "#bfbfb5");
+		jQuery('#summary_link').css("color", "#FFFFFF");
+		jQuery('#summary_link').addClass("active");
 
-		jQuery('#tab3').css("background", "#FFFFFF");
-		jQuery('#tab3').css("color", "#245a83");
+		jQuery('#sample_link').css("background", "#FFFFFF");
+		jQuery('#sample_link').css("color", "#245a83");
 
-		jQuery('#tab4').css("background" ,"#FFFFFF");
-		jQuery('#tab4').css("color", "#245a83");
+		jQuery('#sources_link').css("background" ,"#FFFFFF");
+		jQuery('#sources_link').css("color", "#245a83");*/
+		jQuery('.seller_links').removeClass("active");
+		jQuery('#summary_link').addClass("active");
 	});
-	jQuery("#tab2").click(function(){
-		jQuery("#tabOne").hide();
-		jQuery("#tabTwo").show();
-		jQuery("#tabThree").hide();
-		jQuery("#tabFour").hide();
+	jQuery("#seller_link").click(function(){
+		/*jQuery("#summary_details").hide();
+		jQuery("#about_seller").show();
+		jQuery("#view_sample").hide();
+		jQuery("#sources").hide();*/
+		jQuery(".seller_blocks").hide();
+		jQuery("#about_seller").show();
 
-		jQuery('#tab2').css("background", "#bfbfb5");
-		jQuery('#tab2').css("color", "#FFFFFF");
-		jQuery('#tab2').addClass("active");
+		/*jQuery('#seller_link').css("background", "#bfbfb5");
+		jQuery('#seller_link').css("color", "#FFFFFF");
+		jQuery('#seller_link').addClass("active");
 
-		jQuery('#tab1').css("background" ,"#FFFFFF");
-		jQuery('#tab1').css("color", "#245a83");
+		jQuery('#summary_link').css("background" ,"#FFFFFF");
+		jQuery('#summary_link').css("color", "#245a83");
 
-		jQuery('#tab3').css("background", "#FFFFFF");
-		jQuery('#tab3').css("color", "#245a83");
+		jQuery('#sample_link').css("background", "#FFFFFF");
+		jQuery('#sample_link').css("color", "#245a83");
 
-		jQuery('#tab4').css("background" ,"#FFFFFF");
-		jQuery('#tab4').css("color", "#245a83");
+		jQuery('#sources_link').css("background" ,"#FFFFFF");
+		jQuery('#sources_link').css("color", "#245a83");*/
+		jQuery('.seller_links').removeClass("active");
+		jQuery('#seller_link').addClass("active");
 	});
-	jQuery("#tab3").click(function(){
-		jQuery("#tabOne").hide();
-		jQuery("#tabTwo").hide();
-		jQuery("#tabThree").show();
-		jQuery("#tabFour").hide();
+	jQuery("#sample_link").click(function(){
+		jQuery(".seller_blocks").hide();
+		jQuery("#view_sample").show();
+		/*jQuery("#summary_details").hide();
+		jQuery("#about_seller").hide();
+		jQuery("#view_sample").show();
+		jQuery("#sources").hide();*/
 
-		jQuery('#tab2').css("background", "#FFFFFF");
-		jQuery('#tab2').css("color", "#245a83");
+		/*jQuery('#seller_link').css("background", "#FFFFFF");
+		jQuery('#seller_link').css("color", "#245a83");
 
-		jQuery('#tab1').css("background" ,"#FFFFFF");
-		jQuery('#tab1').css("color", "#245a83");
+		jQuery('#summary_link').css("background" ,"#FFFFFF");
+		jQuery('#summary_link').css("color", "#245a83");
 
-		jQuery('#tab3').css("background", "#bfbfb5");
-		jQuery('#tab3').css("color", "#FFFFFF");
-		jQuery('#tab3').addClass("active");
+		jQuery('#sample_link').css("background", "#bfbfb5");
+		jQuery('#sample_link').css("color", "#FFFFFF");
+		jQuery('#sample_link').addClass("active");
 
-		jQuery('#tab4').css("background" ,"#FFFFFF");
-		jQuery('#tab4').css("color", "#245a83");
+		jQuery('#sources_link').css("background" ,"#FFFFFF");
+		jQuery('#sources_link').css("color", "#245a83");*/
+		jQuery('.seller_links').removeClass("active");
+		jQuery('#sample_link').addClass("active");
 	});
 
-	jQuery("#tab4").click(function(){
-		jQuery("#tabOne").hide();
-		jQuery("#tabTwo").hide();
-		jQuery("#tabThree").hide();
-		jQuery("#tabFour").show();
+	jQuery("#sources_link").click(function(){
+		jQuery(".seller_blocks").hide();
+		jQuery("#sources").show();
+		/*
+		jQuery("#summary_details").hide();
+		jQuery("#about_seller").hide();
+		jQuery("#view_sample").hide();
+		jQuery("#sources").show();
+		*/
+		/*jQuery('#seller_link').css("background", "#FFFFFF");
+		jQuery('#seller_link').css("color", "#245a83");
 
-		jQuery('#tab2').css("background", "#FFFFFF");
-		jQuery('#tab2').css("color", "#245a83");
+		jQuery('#summary_link').css("background" ,"#FFFFFF");
+		jQuery('#summary_link').css("color", "#245a83");
 
-		jQuery('#tab1').css("background" ,"#FFFFFF");
-		jQuery('#tab1').css("color", "#245a83");
+		jQuery('#sample_link').css("background" ,"#FFFFFF");
+		jQuery('#sample_link').css("color", "#245a83");
 
-		jQuery('#tab3').css("background" ,"#FFFFFF");
-		jQuery('#tab3').css("color", "#245a83");
-
-		jQuery('#tab4').css("background", "#bfbfb5");
-		jQuery('#tab4').css("color", "#FFFFFF");
-		jQuery('#tab4').addClass("active");
+		jQuery('#sources_link').css("background", "#bfbfb5");
+		jQuery('#sources_link').css("color", "#FFFFFF");*/
+		jQuery('.seller_links').removeClass("active");
+		jQuery('#sources_link').addClass("active");
 	});
 	jQuery(".fbFooterBorder").css("display","none");
 });
