7afbcea09de5edbda1388bc711de522db4947554
Usman Ali <usman.ali@d3velopers.com>
2011-05-06 17:36:20 +0500

    moved key individual updates to model

--- app/models/key_individual.rb
+++ app/models/key_individual.rb
@@ -2,4 +2,13 @@ class KeyIndividual < ActiveRecord::Base
 
   belongs_to :profile
 
+  def update_key_individual details
+    self.update_attributes details
+    self.update_linkedin self.linkedin
+  end
+
+  def update_linkedin linkedin_profile
+    linkedin = linkedin_profile.match("http")? linkedin_profile : "http://"+ linkedin_profile
+    self.update_attribute( :linkedin , linkedin )
+  end
 end
