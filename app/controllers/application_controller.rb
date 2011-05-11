203fc7bfc40c1cea1aea4f5252e55a4fbaf97157
Ali Ibrahim <aliibrahim@gmail.com>
2011-05-11 20:16:35 +0500

    removed method missing function for application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -68,11 +68,6 @@ class ApplicationController < ActionController::Base
   end
 
 
-  def method_missing(methodname, *args)
-    @methodname = methodname
-    @args = args
-    render "#{RAILS_ROOT}/public/404.html", :status => 404
-  end
 
   def render_404(exception = nil)
     if exception
