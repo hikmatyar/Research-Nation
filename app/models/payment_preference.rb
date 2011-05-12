8a4f5fbaefb091f9d8d9e0a320db672777cdacaf
Ali Ibrahim <aliibrahim@gmail.com>
2011-05-13 02:00:45 +0500

    only show payment preference either address or paypal whichever is selected

--- app/models/payment_preference.rb
+++ app/models/payment_preference.rb
@@ -5,4 +5,12 @@ class PaymentPreference < ActiveRecord::Base
     self.update_attributes(:paypal => preference[:paypal], :address => preference[:address], :option => preference[:option])
   end
 
+  def paypal?
+    self.option == "Paypal"
+  end
+
+  def address?
+    self.option == "Cheque"
+  end
+
 end
