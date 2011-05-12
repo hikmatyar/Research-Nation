a54497aea67cb0c1889ef60dae5961a867643eba
Usman Ali <usman.ali@d3velopers.com>
2011-05-12 16:22:31 +0500

    added popup window script in purchase page and fixed js on purchase page

--- public/javascripts/purchases.js
+++ public/javascripts/purchases.js
@@ -10,14 +10,13 @@ jQuery(document).ready(function(){
 
     });
 
-   if (document.getElementById("terms_link"))
-   {
-     document.getElementById("terms_link").onclick = show_terms;
-   }
-
-   function show_terms()
-   {
-     window.open("/resources/user_terms_and_conditions/<%= @resource.id %>","terms_and_conditions","location=1,status=1,scrollbars=1, width=250,height=250, screenX=900, screenY=250");
-   }
+    jQuery("#order_first_name").click(function(){
+        jQuery(this).val("");
+        jQuery(this).unbind("click");
+    });
+    jQuery("#order_last_name").click(function(){
+        jQuery(this).val("");
+        jQuery(this).unbind("click");
+    });
 
 });
