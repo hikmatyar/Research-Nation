7b2a9b1e70d5a20a79158d6c8e74468e1af9ddc2
Usman Ali <usman.ali@d3velopers.com>
2011-05-13 16:47:54 +0500

    formatted emails and removed unused emails

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -14,7 +14,7 @@ class UserMailer < ActionMailer::Base
   def password_reset_email(user, token)
 
       recipients   user.email
-      subject      "ResearchNation - Password Reset"
+      subject      "Research Nation - Password Reset"
       from         "no-reply@researchnation.net"
       content_type "text/html"
       body         :email => user.email,
@@ -24,7 +24,7 @@ class UserMailer < ActionMailer::Base
 
   def successful_purchase_email(current_user, resource)
       recipients   current_user.email
-      subject      "ResearchNation - You have successfully purchased #{resource.title}"
+      subject      "Research Nation - You have successfully purchased #{resource.title}"
       from         "no-reply@researchnation.net"
       content_type "text/plain"
       body         :user => current_user,
@@ -35,7 +35,7 @@ class UserMailer < ActionMailer::Base
 
   def payment_sent_email(user, start_time, end_time, earnings)
       recipients   user.email
-      subject      "ResearchNation - Payment sent of US$ #{earnings}"
+      subject      "Research Nation - Payment sent of US$ #{earnings}"
       from         "no-reply@researchnation.net"
       bcc           "admin@researchnation.net"
       content_type "text/plain"
