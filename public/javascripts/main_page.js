4f4cc7afe45f766f0253a8645500f85be512bb85
Ali Ibrahim <aliibrahim@gmail.com>
2011-05-14 10:59:53 +0500

    fixed the home page form to retain values and the contact form

--- public/javascripts/main_page.js
+++ public/javascripts/main_page.js
@@ -1,12 +1,26 @@
 $(document).ready(function(){
 
-	$("input").keypress(function(){
-    $(this).siblings().text("");
-    $(this).siblings().css("padding","8px");
-	})
-	$("#new_user").submit(function(){
-		return validate_form();
-	});
+	if ($("#new_user")) {
+    var prevValue = "";
+
+	  $("#new_user").find(":input").focus(function(){
+      prevValue = $('label[for='+$(this).attr('id')+']').text();
+
+      $('label[for='+$(this).attr('id')+']').text("");
+      $('label[for='+$(this).attr('id')+']').css("padding","8px");
+	  });
+
+    $("#new_user").find(":input").blur(function(){
+      if ($(this).val() == ""){
+        $('label[for='+$(this).attr('id')+']').css("padding","0px");
+        $('label[for='+$(this).attr('id')+']').text(prevValue);
+      }
+	  });
+
+	  $("#new_user").submit(function(){
+		  return validate_form();
+	  });
+	}
 
 });
 
