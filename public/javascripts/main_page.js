b0d482a07f2928214f2f61e9c2230f907fe26eb1
Ali Ibrahim <aliibrahim@gmail.com>
2011-05-20 13:24:02 +0500

    removed garbled text errors when on main page the fields are already filled in

--- public/javascripts/main_page.js
+++ public/javascripts/main_page.js
@@ -1,21 +1,44 @@
 $(document).ready(function(){
 
 	if ($("#new_user")) {
-    var prevValue = "";
+    var email_label = "Your Email";
+    var pass_label  = "Your Password";
 
 	  $("#new_user").find(":input").focus(function(){
-      prevValue = $('label[for='+$(this).attr('id')+']').text();
-
-      $('label[for='+$(this).attr('id')+']').text("");
       $('label[for='+$(this).attr('id')+']').css("padding","8px");
+      $('label[for='+$(this).attr('id')+']').text("");
+
 	  });
 
     $("#new_user").find(":input").blur(function(){
       if ($(this).val() == ""){
+        if ($(this).attr('id') == "email"){
+          $('label[for='+$(this).attr('id')+']').text(email_label);
+        }
+        else{
+          $('label[for='+$(this).attr('id')+']').text(pass_label);
+        }
         $('label[for='+$(this).attr('id')+']').css("padding","0px");
-        $('label[for='+$(this).attr('id')+']').text(prevValue);
       }
 	  });
+
+    if ($("input:text").val() != "") {
+      $('label[for='+ $("input:text").attr('id') +']').css("padding","8px");
+      $('label[for='+ $("input:text").attr('id') +']').text("");
+    }
+    else{
+      $('label[for='+ $("input:text").attr('id')+']').text(email_label);
+      $('label[for='+ $("input:text").attr('id')+']').css("padding","0px");
+    }
+
+    if ($("input:password").val() != "") {
+      $('label[for='+ $("input:password").attr('id') +']').css("padding","8px");
+      $('label[for='+ $("input:password").attr('id') +']').text("");
+    }
+    else{
+      $('label[for='+ $("input:password").attr('id') +']').text(pass_label);
+      $('label[for='+ $("input:password").attr('id')+']').css("padding","0px");
+    }
 
 	  $("#new_user").submit(function(){
 		  return validate_form();
