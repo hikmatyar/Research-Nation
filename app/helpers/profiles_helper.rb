8ebaded987d83e3374b3200de3fdea33df92101d
Ali Ibrahim <aliibrahim@gmail.com>
2011-07-02 18:33:43 +0500

    fixed the date issue in the inbox

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,2 +1,9 @@
 module ProfilesHelper
+
+  def days_ago(prev_date)
+    days = ((Time.now - prev_date)/1.day).to_i
+    return "Today" if days == 0
+    return  "1 day ago" if days == 1
+    return  "#{days} days ago"
+  end
 end
